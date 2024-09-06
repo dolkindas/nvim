faa094d8009bcef322678dceedae86d18c075786
Dolkindas <dolkindas@proton.me>
2024-09-06 07:59:46 +0545

    add init lua

---
+++ init.lua
@@ -0,0 +1,114 @@
+
+
+-------------------------------------------------------------------------------------------------------
+-- This initializes the lazy vim which is used for installing plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+--------------------------------------------------------------------------------------------------------
+
+
+-- Remap the leader to space key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Set the tab width to 2 spacesg
+vim.opt.tabstop = 2         -- Number of spaces a tab counts for
+vim.opt.softtabstop = 2     -- Number of spaces to use for <BS> and <Tab>
+vim.opt.shiftwidth = 2      -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+
+-- Some extra steps to get good colors
+vim.o.termguicolors = true
+vim.o.background = 'light'
+
+require("lazy").setup({
+  spec = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    {"rcarriga/nvim-notify", opts = {} },
+    { "stevearc/oil.nvim", opts = {} },
+    {
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+    "shaunsingh/solarized.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+    },
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+  },
+})
+
+-- can only set the color scheme once the plugin is initialized
+vim.cmd.colorscheme 'solarized'
+
+
+-- telescope key bindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- crazy file manager
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- lsp config
+local lsp_zero = require('lsp-zero')
+
+
+local mason_lsp_config = require('mason-lspconfig')
+local mason = require("mason")
+
+
+
+-- lsp_attach is where you enable features that only work
+-- if there is a language server active in the file
+local lsp_attach = function(client, bufnr)
+  local opts = {buffer = bufnr}
+
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+mason.setup()
+mason_lsp_config.setup({
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
+
+local notify = require("notify")
+vim.notify = notify
